80fd04d6ff6d7ef915372f6290d9c9c52ae13dbe
Daniel Ciborowski <dciborow@microsoft.com>
2020-02-18 16:58:05 -0400

    Initial commit

---
+++ tests/test_notebooks.py
@@ -0,0 +1,19 @@
+"""
+ai-architecture-template - test_notebooks.py
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+import pytest
+
+from azure_utils.dev_ops.testing_utilities import run_notebook
+
+from notebooks import DIRECTORY
+
+
+@pytest.mark.parametrize(
+    "notebook",
+    ['00_AMLConfiguration.ipynb'],
+)
+def test_notebook(notebook, add_nunit_attachment):
+    run_notebook(notebook, add_nunit_attachment, kernel_name="ai-architecture-template", root=DIRECTORY)
